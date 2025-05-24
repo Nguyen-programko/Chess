2bf1a881dbc766b5aef7a0b82cb5578bc7dfabb1
Nguyen-programko <nguyenpetr2005@gmail.com>
2025-05-24 21:37:34 +0200

    Add files via upload

---
+++ šachys 2 - new ai/šachys/TAH.cs
@@ -0,0 +1,12 @@
+﻿namespace šachys
+{
+    class TAH
+    {
+        public int[] From;
+        public int[] To;
+        public TAH(int[] from, int[] to)
+        {
+            From = from; To = to;
+        }
+    }
+}
